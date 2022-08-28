69d81ea7d812b5117566a0c3582e70a172eaed32
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-08-28 20:16:43 -0300

    finalized project

--- especializar/reactjs/start/react-app/src/pages/Home/index.jsx
+++ especializar/reactjs/start/react-app/src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Card } from '../../components/Card';
 
 import './style.css';
@@ -5,11 +6,50 @@ import './style.css';
 
 export function Home() {
   const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState([]); 
+  const [user, setUser] = useState({ name: '', avatar: '' });
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    }
+
+    setStudents(prevState => [...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+
+    async function fetchData() {
+      const response = await fetch("https://api.github.com/users/msNullus");
+      const { name, avatar_url } = await response.json();
+      console.log("DADOS => ", name, " | ", avatar_url);
+
+      setUser({
+        name: name,
+        avatar: avatar_url
+      });
+    }
+    
+    fetchData();
+    
+  }, []);
 
 
   return (
-    <div className="container">
-      <h1>Lista de Presença</h1>
+    <main className="container">
+      <header>
+        <h1>Lista de Presença</h1>
+
+        <div>
+          <strong>Ms. Nullus</strong>
+          <img src="https://github.com/msNullus.png" alt="Foto de Perfil" />
+        </div>
+      </header>
 
       <input 
         type="text" 
@@ -17,25 +57,21 @@ export function Home() {
         onChange={( e ) => setStudentName(e.target.value)}
       />
 
-      <button type="button">
+      <button 
+        type="button" 
+        onClick={handleAddStudent}
+      >
         Adicionar
       </button>
 
-      <Card 
-        name="Gustavo H. J." 
-        time="00:00:00" 
-      />
-
-      <Card 
-        name="Klateia" 
-        time="12:23:45" 
-      />
-
-      <Card 
-        name="Kratus" 
-        time="23:34:56" 
-      />
-    </div>
+      {students.map(({ name, time }) => (
+        <Card 
+          key={time}
+          name={name} 
+          time={time} 
+        />
+      ))}
+    </main>
   );
 
 }
