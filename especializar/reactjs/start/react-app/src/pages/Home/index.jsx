d5995132413f74e90c389622540087e85c2f7b5c
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-08-28 02:24:59 -0300

    add and changes components

--- especializar/reactjs/start/react-app/src/pages/Home/index.jsx
+++ especializar/reactjs/start/react-app/src/pages/Home/index.jsx
@@ -1,13 +1,40 @@
-import './styles/global.css';
+import { Card } from '../../components/Card';
+
+import './style.css';
+
 
 export function Home() {
+  const [studentName, setStudentName] = useState("");
+
 
   return (
-    <div className="conainer">
+    <div className="container">
       <h1>Lista de Presença</h1>
 
-      <input type="text" placeholder="Digite o nome..." />
-      <button type="button">Adicionar</button>
+      <input 
+        type="text" 
+        placeholder="Digite o nome..." 
+        onChange={( e ) => setStudentName(e.target.value)}
+      />
+
+      <button type="button">
+        Adicionar
+      </button>
+
+      <Card 
+        name="Gustavo H. J." 
+        time="00:00:00" 
+      />
+
+      <Card 
+        name="Klateia" 
+        time="12:23:45" 
+      />
+
+      <Card 
+        name="Kratus" 
+        time="23:34:56" 
+      />
     </div>
   );
 
